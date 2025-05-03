c00860a8541c835b1e02ffe6dfca7fa582c0b215
MurtazaKolachi <murtazakolachi7@gmail.com>
2025-05-03 09:36:01 +0500

    apollo: Project Matrixx bringup

--- lineage_apollo.mk
+++ lineage_apollo.mk
@@ -11,6 +11,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+WITH_GMS := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_STOCK_AICORE := true
+TARGET_SUPPORTS_WALLEFFECT := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+ 
+# Pixel Launcher
+TARGET_INCLUDE_NEXUS := true
+ 
+# Maintainer
+MATRIXX_MAINTAINER := Murtaza
+
 # Inherit from apollo device
 $(call inherit-product, device/xiaomi/apollo/device.mk)
 
@@ -25,3 +37,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="apollo_global-user 12 RKQ1.211001.001 V14.0.4.0.SJDMIXM release-keys" \
     BuildFingerprint=Xiaomi/apollo_global/apollo:12/RKQ1.211001.001/V14.0.4.0.SJDMIXM:user/release-keys
+
+# Private
+# include vendor/sign/keys/keys.mk
