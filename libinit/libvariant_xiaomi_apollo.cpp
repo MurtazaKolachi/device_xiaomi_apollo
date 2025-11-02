a80bdd73fb79b57f8bdcfe297ff10fa4e9c5e41b
Henrique Pereira <hlcpereira@pixelexperience.org>
2025-11-02 14:21:56 +0500

    Apollo: Drop build fingerprint override
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- libinit/libvariant_xiaomi_apollo.cpp
+++ libinit/libvariant_xiaomi_apollo.cpp
@@ -16,7 +16,7 @@ static const variant_info apollo_global_info = {
     .device = "apollo",
     .marketname = "Mi 10T",
     .model = "M2007J3SY",
-    .build_fingerprint = "Xiaomi/apollo_global/apollo:12/RKQ1.211001.001/V14.0.4.0.SJDMIXM:user/release-keys",
+//    .build_fingerprint = "Xiaomi/apollo_global/apollo:12/RKQ1.211001.001/V14.0.4.0.SJDMIXM:user/release-keys",
 
     .nfc = true,
 };
@@ -31,7 +31,7 @@ static const variant_info apollopro_info = {
     .device = "apollo",
     .marketname = "Mi 10T Pro",
     .model = "M2007J3SG",
-    .build_fingerprint = "Xiaomi/apollopro_global/apollo:12/RKQ1.211001.001/V14.0.4.0.SJDMIXM:user/release-keys",
+//    .build_fingerprint = "Xiaomi/apollopro_global/apollo:12/RKQ1.211001.001/V14.0.4.0.SJDMIXM:user/release-keys",
 
     .nfc = true,
 };
@@ -46,7 +46,7 @@ static const variant_info apollo_info = {
     .device = "apollo",
     .marketname = "Redmi K30S Ultra",
     .model = "M2007J3SC",
-    .build_fingerprint = "Redmi/apollo/apollo:12/RKQ1.211001.001/V14.0.5.0.SJDCNXM:user/release-keys",
+//    .build_fingerprint = "Redmi/apollo/apollo:12/RKQ1.211001.001/V14.0.5.0.SJDCNXM:user/release-keys",
 
     .nfc = true,
 };
