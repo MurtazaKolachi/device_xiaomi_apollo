8afb19e1efe8941a224ea3399405df4c47644503
Murtaza Hussain <160129300+MurtazaKolachi@users.noreply.github.com>
2025-12-04 18:41:43 +0500

    Apollo: Correct kernel path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -64,11 +64,7 @@ BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_SOURCE := kernel/xiaomi/apollo
-TARGET_KERNEL_CONFIG := \
-    vendor/kona-perf_defconfig \
-    vendor/debugfs.config \
-    vendor/xiaomi/sm8250-common.config \
-    vendor/xiaomi/apollo.config
+TARGET_KERNEL_CONFIG := vendor/apollo_defconfig
 
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := apollo,apollopro
