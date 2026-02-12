471be8c608be8f3d3d368f5398317198a894b4b1
Murtaza Hussain <160129300+MurtazaKolachi@users.noreply.github.com>
2026-02-12 14:40:29 +0500

    Apollo: Welcome @The-Clover-Project

--- clover_apollo.mk
+++ clover_apollo.mk
@@ -8,13 +8,22 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Clover stuff.
+$(call inherit-product, vendor/clover/config/common_full_phone.mk)
 
 # Inherit from apollo device
 $(call inherit-product, device/xiaomi/apollo/device.mk)
 
-PRODUCT_NAME := lineage_apollo
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Clover assortment
+TARGET_INCLUDE_PIXEL_LAUNCHER := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_SUPPORTS_QUICK_TAP := true
+
+PRODUCT_NAME := clover_apollo
 PRODUCT_DEVICE := apollo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
