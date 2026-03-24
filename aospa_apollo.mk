413a712d9b0c89c15f69b75722eac04c9f427da3
Murtaza Hussain <160129300+MurtazaKolachi@users.noreply.github.com>
2026-03-24 15:12:03 +0000

    apollo: Add missing TARGET_BOOT_ANIMATION_RES
    Fixes build error: 'Invalid bootanimation resolution: . Define TARGET_BOOT_ANIMATION_RES to 720/1080/1440/2160.'

--- aospa_apollo.mk
+++ aospa_apollo.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Paranoid stuff.
-$(call inherit-product, vendor/aospa/target/product/aospa_mobile.mk)
+$(call inherit-product, vendor/aospa/target/product/aospa-mobile.mk)
 
 # Inherit from apollo device
 $(call inherit-product, device/xiaomi/apollo/device.mk)
@@ -23,6 +23,8 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Xiaomi Mi 10T
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="apollo_global-user 12 RKQ1.211001.001 V14.0.4.0.SJDMIXM release-keys" \
