krastinator/android_device_xiaomi_apollo

Apollo: Inherit Miui Camera Repository
- Includes MiSys and cleaned deps
- https://gitlab.com/johnmart19/vendor_xiaomi_camera

device.mk
@@ -244,6 +244,9 @@ PRODUCT_PACKAGES += \
     media_codecs_performance_kona.xml \
     video_system_specs.json
 
+# Miui Camera
+include vendor/xiaomi/camera/miuicamera.mk
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc-service.nxp \

MurtazaKolachi/device_xiaomi_apollo

device.mk
@@ -316,6 +316,9 @@ PRODUCT_COPY_FILES += \
 # OMX
 TARGET_SUPPORTS_OMX_SERVICE := false
 
+# Miui Camera
+include device/xiaomi/camera/miuicamera.mk
+
 # Overlays
 PRODUCT_PACKAGES += \
     CarrierConfigOverlayCommon \